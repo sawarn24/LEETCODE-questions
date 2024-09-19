5c0b13a39f57a770b5ded7ad223dc0c3ee8304e0
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-09-19 07:28:41 +0530

    Time: 0 ms (100%), Space: 7.4 MB (10.22%) - LeetHub

---
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int n) {
+        if(n==0)
+        return true;
+        if(n%10==0)
+        return false;
+        return true;
+        
+    }
+};
