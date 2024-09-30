96b094aff90897859b1b3c577261e9236f3d357f
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-09-30 21:41:35 +0530

    Time: 106 ms (79.94%), Space: 99.2 MB (65.05%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true, decreasing = true;
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[i - 1]) {
+            decreasing = false;
+        }
+        if (nums[i] < nums[i - 1]) {
+            increasing = false;
+        }
+         if (!increasing && !decreasing) {
+            return false;
+        }
+    }
+
+    return true;
+}
+        
+    
+};
