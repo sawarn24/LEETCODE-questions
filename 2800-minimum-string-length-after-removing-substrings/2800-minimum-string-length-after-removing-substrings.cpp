09248922f98bfaf27ce57a61e2963fbc1d6f0ee1
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-10-07 22:20:47 +0530

    Time: 9 ms (24.64%), Space: 11.6 MB (31.24%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(auto c: s)
+        {
+            if (!st.empty() && st.top() == 'A' && c == 'B') {
+                st.pop();
+            }
+           else if (!st.empty() && st.top() == 'C' && c == 'D') {
+                st.pop();
+            }
+             else {
+                
+                st.push(c);
+            }
+            
+        }
+        return st.size();
+        
+    }
+};
