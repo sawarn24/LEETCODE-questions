c50c2acfcabf1d31ccb69756d0af8e129b5ab307
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-10-18 23:18:43 +0530

    Time: 446 ms (45.03%), Space: 11.7 MB (77.2%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) {
+        return 0;
+    }
+
+    
+    vector<bool> is_prime(n, true);
+    is_prime[0] = is_prime[1] = false;  
+   
+    for (int i = 2; i * i < n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+   
+    int prime_count = 0;
+    for (int i = 2; i < n; ++i) {
+        if (is_prime[i]) {
+            ++prime_count;
+        }
+    }
+
+    return prime_count;
+        
+    }
+};
