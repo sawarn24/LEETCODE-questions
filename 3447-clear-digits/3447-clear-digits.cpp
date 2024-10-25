91a50b6cd9ab6aa83bf65a47640d9daee432c411
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-10-25 23:54:31 +0530

    Time: 3 ms (40.64%), Space: 8.8 MB (38.84%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+               
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else {
+                
+                st.push(c);
+            }
+        }
+
+       
+        string res;
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+
+        
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
