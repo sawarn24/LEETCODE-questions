b38a1f76fbd46708fa58d71c5cb7619f251f83dc
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2025-01-25 01:19:44 +0530

    Time: 2 ms (84.3%), Space: 39.5 MB (91.76%) - LeetHub

--- 2722-prime-in-diagonal/2722-prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-bool isPrime(int num) {
-    if (num <= 1) return false;  // 1 or smaller are not prime
-    if (num == 2) return true;   // 2 is prime
-    if (num % 2 == 0) return false; // Even numbers greater than 2 are not prime
-    for (int i = 3; i * i <= num; i += 2) {  // Check only odd divisors up to sqrt(num)
-        if (num % i == 0) {
-            return false;
-        }
+   
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) return false;
     }
     return true;
 }
-    int diagonalPrime(vector<vector<int>>& nums) {
-        int n = nums.size();
-    int largestPrime = 0;
 
-    // Check primary and secondary diagonals
+// Function to find the largest prime number on the diagonals
+ int diagonalPrime(vector<vector<int>>& nums) {
+    int n = nums.size();
+    int m = nums[0].size();
+    int maxPrime = 0;
+
     for (int i = 0; i < n; ++i) {
-        // Primary diagonal: nums[i][i]
-        if (isPrime(nums[i][i])) {
-            largestPrime = max(largestPrime, nums[i][i]);
+        // Check main diagonal element
+        if (i < m && isPrime(nums[i][i])) {
+            maxPrime = max(maxPrime, nums[i][i]);
         }
 
-        // Secondary diagonal: nums[i][n - i - 1]
-        if (isPrime(nums[i][n - i - 1])) {
-            largestPrime = max(largestPrime, nums[i][n - i - 1]);
+        // Check anti-diagonal element
+        if (i < m && isPrime(nums[i][m - i - 1])) {
+            maxPrime = max(maxPrime, nums[i][m - i - 1]);
         }
     }
 
-    return largestPrime;
+    return maxPrime;
         
     }
 };
