04a1b248b4e99ae2cc607baabcdd99e7a9bb7fb1
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2025-02-24 00:41:42 +0530

    Time: 3 ms (30.56%), Space: 28.2 MB (58.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt=1;
-        int max=nums[0];
-        int maxtime=1;
-        int i;
         sort(nums.begin(),nums.end());
-        for(i=1;i<size(nums);i++)
-        {
-            if(nums[i]==nums[i-1])
-            {
-                cnt++;
-            }
-            else{
-                if(cnt>maxtime)
-                {
-                    maxtime=cnt;
-                    max=nums[i-1];
-                }
-                cnt=1;
-            }
-        }
-      return ( cnt<maxtime)? max:nums[size(nums)-1];
+        int n=nums.size()/2;
+        return nums[n];
     }
 };
